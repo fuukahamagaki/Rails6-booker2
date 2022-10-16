49283851ce76b2f765c201107849eddcde87e218
fuuka hamagaki <sw.frim@gmail.com>
2022-10-16 06:42:43 +0000

    9c

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
   resources :rooms, only: [:create, :show, :index]
   resources :groups do
     get "join" => "groups#join"
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
   end
 
 
